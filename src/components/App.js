85f23e2c9aa7aa71b9cd100d9d96d788f47358d6
Szymon Guzik <szymon198198@gmail.com>
2019-03-01 12:19:08 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="app">
+				<header className="app--header">
+					<h1>Form Builder</h1>
+				</header>
+
+				<ul className="app--list" />
+			</div>
+		);
+	}
+}
+
+export default App;
